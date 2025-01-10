f9a78f7ba49d2826702e622e9198b76f2f80fa62
Ernest Manukyan <erik.manukyan2010@yandex.ru>
2025-01-10 16:58:00 +0400

    chore: update projects page

--- app/pages/projects/[...project].vue
+++ app/pages/projects/[...project].vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 const route = useRoute('projects-project')
+const { t } = useI18n()
 
 if (!route.params.project) {
   throw createError({ statusCode: 404, statusMessage: 'Page Not Found' })
@@ -7,12 +8,13 @@ if (!route.params.project) {
 </script>
 
 <template>
-  <NuxtLink class="mb-3" to="/projects">
-    Back to projects
+  <NuxtLink class="group mb-3 w-fit inline-flex items-center gap-4 p-1.5" to="/projects">
+    <span class="i-mdi-keyboard-backspace inline-block text-2xl transition-transform group-hover:-translate-x-5px" />
+    {{ t('toBack') }}
   </NuxtLink>
 
   <ContentDoc v-slot="{ doc }">
-    <ContentRenderer :value="doc" class="mdc material-theme" />
+    <ContentRenderer :value="doc" class="mdc" />
   </ContentDoc>
 </template>
 
@@ -63,7 +65,8 @@ if (!route.params.project) {
     @apply border-primary border-x-4 pl-4 py-2 bg-accent;
   }
 
-  li > code {
+  li > code,
+  p > code {
     @apply bg-accent p-1 rounded-md text-sm;
   }
 
@@ -76,3 +79,14 @@ if (!route.params.project) {
   }
 }
 </style>
+
+<i18n lang="json">
+{
+	"en": {
+		"toBack": "Back to projects"
+	},
+	"ru": {
+		"toBack": "Назад к проектам"
+	}
+}
+</i18n>
